5022a6610bf1f40a703b78ca26618847c51603d4
unknown <degrookp@S205700.alfa.top.vlaanderen.be>
2016-04-05 10:31:52 +0200

    update

--- R/Erfgoed_theme.R
+++ R/Erfgoed_theme.R
@@ -23,11 +23,11 @@ theme_erfgoed <- function(base_size = 12, base_family = get_default_font()) {
           panel.grid.minor.y = ggplot2::element_line(size= 0.3, colour = "#b3b3b3", linetype = "dotted"),
           panel.grid.minor.x = ggplot2::element_line(size= 0.3, colour = "#b3b3b3", linetype = "dotted"),
 
-          plot.title = ggplot2::element_text(size = 15, face= "bold", margin =  ggplot2::margin(20,20,20,20)),
+          plot.title = ggplot2::element_text(size = 15, face= "bold", margin = margin(20,20,20,20)),
 
           axis.text = ggplot2::element_text (size= 12),
-          axis.title.y = ggplot2::element_text(angle=90, size= 12, face = "bold", margin = ggplot2::margin(0,15,0,0)),
-          axis.title.x = ggplot2::element_text(size= 12, face = "bold", margin = ggplot2::margin(15,0,0,0)),
+          axis.title.y = ggplot2::element_text(size= 12, face = "bold",angle = 90, margin = margin(1,15,1,1)),
+          axis.title.x = ggplot2::element_text(size= 12, face = "bold", margin = margin(15,1,1,1)),
 
           legend.position=("bottom"),
           legend.title = ggplot2::element_blank(),
